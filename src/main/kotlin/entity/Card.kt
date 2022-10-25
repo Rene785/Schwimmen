9b003a419a6587323dbd1b14d9a067a3cc27050a
rene.greff <rene.greff@tu-dortmund.de>
2022-10-25 15:42:29 +0200

    Basic Structure of Entity Layer established.

---
+++ src/main/kotlin/entity/Card.kt
@@ -0,0 +1,6 @@
+package entity
+
+data class Card(val color:CardSuit,val value: CardValue,val state:CardState) {
+
+
+}
---
+++ src/main/kotlin/entity/CardState.kt
@@ -0,0 +1,10 @@
+package entity
+
+enum class CardState {
+    MIDDLE,
+    DRAW_STACK,
+    ON_PLAYER_HAND,
+    OUT_OF_GAME,
+    ;
+
+}
