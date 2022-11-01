5293770dadb77b9e713a68d83990da85a94f5098
Rene785 <rene.greff@arcor.de>
2022-11-01 20:00:49 +0100

    Classes Game and Player are now initializing some values statically where it makes sense instead of initializing them in the constructor.

--- src/main/kotlin/entity/Game.kt
+++ src/main/kotlin/entity/Game.kt
@@ -2,10 +2,18 @@ package entity
 
 /**
  * Entity class [Game] saves the current player, the pass counter, a list of the players and a card Array where all cards are saved in.
- * @constructor [Game] initializes the [currentPlayer],[passCounter],[playerList],[cardArr].
- * @param currentPlayer resembles the current player.
- * @param passCounter represents the counter of how many times the players have pressed pass in a row. (Value: 0-4)
+ * @constructor [Game] initializes the [playerList],[cardArr].
  * @param playerList is the list of all players participating in the round.
  * @param cardArr is an Array of all cards in the Deck.
  */
-data class Game(var currentPlayer:Iterator<Player>,var passCounter: Int, val playerList:List<Player>, var cardArr:Array<Card> )
+data class Game(val playerList:List<Player>, var cardArr:Array<Card> ){
+    /**
+     * Counter to count how many times the players have passed in a row
+     */
+    var passCounter = 0
+    /**
+     * Iterator to iterate over the player list to determine the current player
+     */
+    var currentPlayer = playerList.listIterator()
+}
+
--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -2,11 +2,10 @@ package entity
 
 /**
  * Entity Class [Player] saves the name, the hand Cards of the player and whether the Player has knocked or not.
- * @constructor [Player] initializes a player with his name whether he has knocked and his hand cards.
+ * @constructor [Player] initializes a player with his name and his hand cards.
  * @param name Name of the player
- * @param hasKnocked True when the player has knocked
  * @param handArr Array of all three hand cards of the player.
  */
-data class Player(val name:String, var hasKnocked:Boolean, var handArr: Array<Card>? = null){
-
+data class Player(val name:String, var handArr: Array<Card>){
+    var hasKnocked = false
 }
