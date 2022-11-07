567b9922d23337a6cb0bfbc32d59bfcd5ab23638
rene.greff <rene.greff@tu-dortmund.de>
2022-11-07 16:00:39 +0100

    Interface Refreshables has been created and implemented.

---
+++ src/main/kotlin/view/Refreshables.kt
@@ -0,0 +1,11 @@
+package view
+
+interface Refreshables {
+    fun refreshHandCards()
+    fun refreshMiddleCards()
+    fun refreshScore()
+    fun refreshAfterKnocking()
+    fun refreshAfterPass()
+    fun refreshCreatePlayer()
+    fun refreshAfterNextPlayer()
+}
