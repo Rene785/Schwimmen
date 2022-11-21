e1fa00372077aba18c1d98632b272b3e2144545c
rene.greff <rene.greff@tu-dortmund.de>
2022-11-21 09:43:59 +0100

    Updated function calculatePoints(). Added a toInt() method in CardValue.

--- src/main/kotlin/entity/Player.kt
+++ src/main/kotlin/entity/Player.kt
@@ -1,5 +1,7 @@
 package entity
 
+import java.util.*
+
 /**
  * Entity Class [Player] saves the name, the hand Cards of the player and whether the Player has knocked or not.
  * @constructor [Player] initializes a player with his name and his hand cards.
@@ -8,11 +10,33 @@ package entity
  */
 data class Player(var name:String, var handCardList: MutableList<Card>? = null){
     var hasKnocked = false
-    var points = 0
-    //TODO Methode hängt noch nicht von Farben ab und hat irgendwie mit String ein Problem
+    var points = 0.0
     fun calculatePoints(){
+        var heartScore = 0.0
+        var spadeScore = 0.0
+        var diamondScore = 0.0
+        var clubScore = 0.0
+        var valueScore = 0.0
+        val valueList = LinkedList<CardValue>()
         for(card in handCardList!!) {
-            points += Integer.parseInt(card.value.toString().lowercase())
+            if(card.color.toString() == "♥") heartScore += card.value.toInt().toDouble()
+            if(card.color.toString() == "♦") spadeScore += card.value.toInt().toDouble()
+            if(card.color.toString() == "♣") diamondScore += card.value.toInt().toDouble()
+            if(card.color.toString() == "♠") clubScore += card.value.toInt().toDouble()
+            valueList.add(card.value)
         }
+        if(valueList[0].equals(valueList[1]) && valueList[0].equals(valueList[2])){
+               valueScore = 30.5
+        }
+        if(valueList[0].equals(valueList[1]) && valueScore == 0.0){
+            valueScore = valueList[0].toInt().toDouble() + valueList[1].toInt().toDouble()
+        }
+        if(valueList[0].equals(valueList[2]) && valueScore == 0.0){
+            valueScore = valueList[0].toInt().toDouble() + valueList[2].toInt().toDouble()
+        }
+        if(valueList[1].equals(valueList[2]) && valueScore == 0.0){
+            valueScore = valueList[1].toInt().toDouble() + valueList[2].toInt().toDouble()
+        }
+        points = maxOf(heartScore,spadeScore,diamondScore,clubScore,valueScore)
     }
 }
